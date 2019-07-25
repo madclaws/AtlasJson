aca982a7d14f0c4f8abb600114fc4c29457cbe17
Anandu Pavanan <anandu@gamezop.co>
2019-07-26 02:54:53 +0530

    Update atlasJson.js

--- atlasJson/atlasJson.js
+++ atlasJson/atlasJson.js
@@ -1,45 +1,42 @@
-var writeFile=require('write');
+var writeFile = require('write');
 var jsonStructure;
 //Edit with the sprite's json data location
-var parsedData=require('./../sprites/sprites.json');
+var parsedData = require('./../sprites/sprites.json');
 
 //Edit with location where new Easel format data should be saved
-var dest="spriteAtlasData.json";
+var dest = "spriteAtlasData.json";
 //Edit with spriteSheets location
-var spriteImageDir="sprites/spritesheet.png";
+var spriteImageDir = "sprites/spritesheet.png";
 
 createJson();
 
 writeEaselFormat();
 console.log(jsonStructure);
 
-function createJson()
-{
-jsonStructure="{\n"+"\"images\":["+"\""+spriteImageDir+"\""+"],\n\"frames\":[\n";
+function createJson() {
+    jsonStructure = "{\n" + "\"images\":[" + "\"" + spriteImageDir + "\"" + "],\n\"frames\":[\n";
 
 
-for(i=0;i<parsedData.frames.length;i++)
-{
-	if(i!=parsedData.frames.length-1)
-    jsonStructure+="\t\t["+parsedData.frames[i].frame.x+","+parsedData.frames[i].frame.y+","+parsedData.frames[i].frame.w+","+parsedData.frames[i].frame.h+"],\n";
-	else
-	jsonStructure+="\t\t["+parsedData.frames[i].frame.x+","+parsedData.frames[i].frame.y+","+parsedData.frames[i].frame.w+","+parsedData.frames[i].frame.h+"]\n";	
-}
-jsonStructure+="\t],\n\"animations\":{\n";
-for(i=0;i<parsedData.frames.length;i++)
-{	if(i!=parsedData.frames.length-1)
-    jsonStructure+="\t\t\""+parsedData.frames[i].filename+"\":["+i+"],\n";
-    else
-    	jsonStructure+="\t\t\""+parsedData.frames[i].filename+"\":["+i+"]\n";
-}
-jsonStructure+="\t}\n}";
+    for (i = 0; i < parsedData.frames.length; i++) {
+        if (i != parsedData.frames.length - 1)
+            jsonStructure += "\t\t[" + parsedData.frames[i].frame.x + "," + parsedData.frames[i].frame.y + "," + parsedData.frames[i].frame.w + "," + parsedData.frames[i].frame.h + "],\n";
+        else
+            jsonStructure += "\t\t[" + parsedData.frames[i].frame.x + "," + parsedData.frames[i].frame.y + "," + parsedData.frames[i].frame.w + "," + parsedData.frames[i].frame.h + "]\n";
+    }
+    jsonStructure += "\t],\n\"animations\":{\n";
+    for (i = 0; i < parsedData.frames.length; i++) {
+        if (i != parsedData.frames.length - 1)
+            jsonStructure += "\t\t\"" + parsedData.frames[i].filename + "\":[" + i + "],\n";
+        else
+            jsonStructure += "\t\t\"" + parsedData.frames[i].filename + "\":[" + i + "]\n";
+    }
+    jsonStructure += "\t}\n}";
 }
 
 
-function writeEaselFormat()
-{
-    writeFile(dest,jsonStructure,function(err){
-        if(err)
+function writeEaselFormat() {
+    writeFile(dest, jsonStructure, function(err) {
+        if (err)
             console.log(err);
         else
             console.log("SUCCESS");
